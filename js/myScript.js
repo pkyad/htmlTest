6f7011701b67b0b419ccc4fd415d338302eeb885
Pradeep Yadav <pradeepyadav@iitb.ac.in>
2015-09-05 08:07:21 +0530

    calender fully functional with string parser etc

--- js/myScript.js
+++ js/myScript.js
@@ -21,9 +21,13 @@ ngCal.controller('myCtrl', function($scope) {
   $scope.listOfYears = [{"val":2015, "disp":"2015"}, {"val":2016, "disp":"2016"}, {"val":2017, "disp":"2017"}, {"val":2018, "disp":"2018"}, {"val":2019, "disp":"2019"}];
   $scope.listOfDays = [{"val":1, "disp":"Sunday"}, {"val":1, "disp":"Monday"}, {"val":1, "disp":"Tuesday"}, {"val":1, "disp":"Wednesday"}, {"val":1, "disp":"Thursday"}, {"val":1, "disp":"Friday"}, {"val":1, "disp":"Saturday"}];
 
+  var time1 = new Date('7/9/2015 2:15:34');
+  var time2 = new Date('7/8/2015 4:12:34');
+  var time3 = new Date('7/10/2015 5:11:34');
+
   $scope.listOfToDo = [{"id":1, "flag":"", "title":"Task 1"},{"id":2, "flag":"", "title":"Task 2"},{"id":3, "flag":"", "title":"Task 3"},{"id":4, "flag":"", "title":"Task 4"}];
-  $scope.listOfMeetings = [{"id":1, "title": "Review", "venue": "302", "time": "2:30 pm", "date":"2015-09-04"}, {"id":2,"title": "Project", "venue": "302", "time": "1:30 pm", "date":"2015-09-05"}, {"id":3,"title": "Orientation", "venue": "303", "time": "4:30 pm", "date":"2015-09-05"}];
-  $scope.listOfReminders = [{"id":1,"title": "Assesment", "time": "2:30 pm", "date":"2015-09-04"}, {"id":2,"title": "Submission", "time": "1:30 pm", "date":"2015-09-05"}, {"id":3,"title": "Course", "time": "4:30 pm", "date":"2015-09-05"}];
+  $scope.listOfMeetings = [{"id":1, "title": "Review", "venue": "302", "dayTime":time1.toLocaleTimeString().replace(/:\d+ /, ' '), "date":time1, "duration":1}];
+  $scope.listOfReminders = [{"id":1,"title": "Assesment", "date":time1, "dayTime":time1.toLocaleTimeString().replace(/:\d+ /, ' ')}, {"id":2,"title": "Submission", "date":time2, "dayTime":time2.toLocaleTimeString().replace(/:\d+ /, ' ')}, {"id":3,"title": "Course","date":time3, "dayTime":time3.toLocaleTimeString().replace(/:\d+ /, ' ')}];
   var calDate = new Date(); // the current date value known to the calender, also the selected. For a random month its 1st day of that month.
   var calMonth = calDate.getMonth();
   var calYear = calDate.getFullYear();
@@ -38,7 +42,11 @@ ngCal.controller('myCtrl', function($scope) {
   $scope.dropMonth = {"val":calMonth, "disp":""}; // Month selected in the drop down menu
 
   var initialNumOfToDo = $scope.listOfToDo.length;
+  var initialNumOfReminder = $scope.listOfReminders.length;
+  var initialNumOfMeetings = $scope.listOfMeetings.length;
   var idForNewToDo = initialNumOfToDo + 1;
+  var idForNewReminder = initialNumOfReminder +1;
+  var idForNewMeeting = initialNumOfMeetings +1;
 
   $scope.showDetails = function(val){
     if (datesMap.flags[val]=="Cur"){
@@ -60,10 +68,85 @@ ngCal.controller('myCtrl', function($scope) {
     };
   };
   $scope.addNewToDo = function(){
-
     $scope.listOfToDo.push({"id": idForNewToDo, "title": $scope.newToDo});
     idForNewToDo +=1;
     $scope.newToDo = "";
+  };
+  $scope.addMeeting = function(){
+    str = $scope.meetingEditor;
+    var parts = str.split(" in ");
+    var str1 = parts[0];
+    var str2 = parts[1];
+    var parts2 = str1.split(" at ");
+    var title = parts2[0];
+    var timeStr = parts2[1];
+    dayTimeInd = timeStr.search(/pm/i);
+    dayTime = "pm";
+    if (dayTimeInd==-1){
+      dayTimeInd = timeStr.search(/am/i);
+      dayTime = "am";
+    }
+    var time = timeStr.split("pm", 1);
+    // var n = time[0].length;
+    if(time[0].search(":")==-1){
+      timeHr = parseInt(time[0]);
+      timeMin = 0;
+    }else{
+      timeCom = time[0].split(":");
+      timeHr = parseInt(timeCom[0]);
+      timeMin = parseInt(timeCom[1]);
+    }
+    var tObj = new Date();
+    if (dayTime=="pm"){
+      timeHr +=12;
+    }
+    tObj.setHours(timeHr);
+    tObj.setMinutes(timeMin);
+    var parts3 = str2.split("for");
+    venue = parts3[0];
+    str3 = parts3[1];
+    var temp = str3.split("h");
+    duration = parseFloat(temp[0]);
+    meetingObj = {"id":idForNewMeeting, "title":title,"venue":venue, "dayTime":tObj.toLocaleTimeString().replace(/:\d+ /, ' '), "date":tObj,  "duration":duration};
+    $scope.listOfMeetings.push(meetingObj);
+    $scope.meetingEditor="";
+    idForNewMeeting +=1;
+    // alert(venue+ duration+ title+tObj);
+  };
+  $scope.addReminder = function(){
+
+    str = $scope.reminderEditor;
+    var parts = str.split("by");
+    // var n = str.search(/by/i);
+    var timeStr = parts[1];
+    var title = parts[0];
+    dayTimeInd = timeStr.search(/pm/i);
+    dayTime = "pm";
+    if (dayTimeInd==-1){
+      dayTimeInd = timeStr.search(/am/i);
+      dayTime = "am";
+    }
+    var time = timeStr.split("pm", 1);
+    // var n = time[0].length;
+    if(time[0].search(":")==-1){
+      timeHr = parseInt(time[0]);
+      timeMin = 0;
+    }else{
+      timeCom = time[0].split(":");
+      timeHr = parseInt(timeCom[0]);
+      timeMin = parseInt(timeCom[1]);
+    }
+    var tObj = new Date();
+    if (dayTime=="pm"){
+      timeHr +=12;
+    }
+    tObj.setHours(timeHr);
+    tObj.setMinutes(timeMin);
+    reminderObj = {"id":idForNewReminder, "title":title, "date":tObj, "dayTime":tObj.toLocaleTimeString().replace(/:\d+ /, ' ')};
+    $scope.listOfReminders.push(reminderObj);
+    $scope.reminderEditor = "";
+    idForNewReminder +=1;
+    // alert(title+ ","+ dayTime+ ","+ dayTimeInd + "end");
   };
 
   $scope.todoUpdate = function(id){
@@ -79,13 +162,24 @@ ngCal.controller('myCtrl', function($scope) {
   };
 
   $scope.editItem = function(val, itemType){
-    alert(val + " , "  + itemType +"edit clicked");
+    // alert(val + " , "  + itemType +"edit clicked");
     if (itemType==1){
-
+      angular.forEach($scope.listOfReminders, function(item, key){
+        if (item.id==val){
+          $scope.listOfReminders.splice(key, 1);
+          $scope.reminderEditor = item.title+" by "+item.dayTime;
+        };
+      });
     }else if (itemType==2){
 
-    }else{
+      angular.forEach($scope.listOfMeetings, function(item, key){
+        if (item.id==val){
 
+          $scope.listOfMeetings.splice(key, 1);
+          // alert($scope.listOfMeetings+"hfb");
+          $scope.meetingEditor = item.title+" at "+item.dayTime+" in "+item.venue+" for "+item.duration+" hrs" ;
+        };
+      });
     };
   };
   $scope.deleteItem = function(val, itemType){
